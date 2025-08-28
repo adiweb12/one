42f47ebfa9b90a8e1f81f906aa6b9ccf91317e36
adiweb12 <vsadith051@gmail.com>
2025-08-28 08:26:10 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main() {
   runApp(MyApp());
 }
 
-const String SERVER_IP = "onechatjdifivifrrfigiufitxtd6xyycyft.onrender.com";
+const String SERVER_IP = "100.20.92.101";
 
 class MyApp extends StatelessWidget {
   @override
