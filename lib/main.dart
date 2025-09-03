7c44be43107a9b827d8566e49211e902721ce182
adiweb12 <vsadith051@gmail.com>
2025-09-03 20:35:33 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,44 +1,51 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-import 'login_page.dart';
-import 'profile_page.dart';
+import 'login.dart';
+import 'groups.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  final storage = const FlutterSecureStorage();
+  const MyApp({super.key});
+
+  Future<Map<String, String?>> _checkLogin() async {
+    const storage = FlutterSecureStorage();
+    String? username = await storage.read(key: 'username');
+    String? token = await storage.read(key: 'token');
+
+    if (username != null && token != null) {
+      return {"username": username, "token": token};
+    }
+    return {};
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'One Chat',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: FutureBuilder(
+      home: FutureBuilder<Map<String, String?>>(
         future: _checkLogin(),
         builder: (context, snapshot) {
           if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Scaffold(body: Center(child: CircularProgressIndicator()));
-          } else if (snapshot.hasData && snapshot.data != null) {
-            return ProfilePage(
-              username: snapshot.data!['username'],
-              token: snapshot.data!['token'],
+            return const Scaffold(
+              body: Center(child: CircularProgressIndicator()),
             );
-          } else {
-            return LoginPage();
           }
+
+          if (snapshot.hasData && snapshot.data!.isNotEmpty) {
+            return GroupsPage(
+              username: snapshot.data?['username'] ?? "",
+              token: snapshot.data?['token'] ?? "",
+            );
+          }
+
+          return LoginPage();
         },
       ),
     );
-  }
-
-  Future<Map<String, String>?> _checkLogin() async {
-    String? username = await storage.read(key: 'username');
-    String? token = await storage.read(key: 'token');
-    if (username != null && token != null) {
-      return {"username": username, "token": token};
-    }
-    return null;
   }
 }
