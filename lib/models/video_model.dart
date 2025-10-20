f388b6b1aa2764ae04f42ef8a255a839eae4dcb7
adiweb12 <vsadith051@gmail.com>
2025-10-20 11:17:58 +0530

    Add files via upload

--- lib/models/video_model.dart
+++ lib/models/video_model.dart
@@ -1,1 +1,25 @@
+class VideoModel {
+  final int id;
+  final String title;
+  final String description;
+  final String thumbnailUrl;
+  final String videoUrl;
 
+  VideoModel({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.thumbnailUrl,
+    required this.videoUrl,
+  });
+
+  factory VideoModel.fromJson(Map<String, dynamic> json) {
+    return VideoModel(
+      id: json['id'] is int ? json['id'] : int.parse('${json['id']}'),
+      title: json['title'] ?? '',
+      description: json['description'] ?? '',
+      thumbnailUrl: json['thumbnail_url'] ?? json['thumbnail'] ?? '',
+      videoUrl: json['video_url'] ?? json['video'] ?? '',
+    );
+  }
+}
